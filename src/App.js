ee095aea59997146defe1bbaaa0c742148e3f2d8
hardikshah197 <hardikshah.hs2015@gmail.com>
2020-05-29 14:53:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+  {/* //   <div className="App"> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Hardik!!!
@@ -18,7 +21,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Restonrent Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
     </div>
   );
 }

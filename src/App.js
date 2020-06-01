6057d25d62fd076cad4ba6f19a5e05210a3ac797
hardikshah197 <hardikshah.hs2015@gmail.com>
2020-06-01 15:53:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/menuComponent';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-  {/* //   <div className="App"> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Hardik!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Restonrent Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
-}
+class App extends Component {
 
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    );
+  }
+}
 export default App;

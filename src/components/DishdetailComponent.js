fc930c2b4002e0ee469910d14c6411f1d4e5042b
hardikshah197 <hardikshah.hs2015@gmail.com>
2020-06-01 16:28:10 +0530

    Function Component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { renderIntoDocument } from 'react-dom/test-utils';
 
-class Dishdetail extends Component {
-
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -32,7 +31,7 @@ class Dishdetail extends Component {
         )
     }
 
-    renderDish(dish) {
+    function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -51,22 +50,19 @@ class Dishdetail extends Component {
         }
     }
 
-    render() {
-        const dish = this.props.dish
+    const DishDetail = (props) => {
+        const dish = props.dish
         if (dish == null) {
             return (<div></div>)
         }
-        const dishItem = this.renderDish(dish)
-        const commentItem = this.renderComments(dish.comments)
         return (
             <div className='container'>
                 <div className='row'>
-                    {dishItem}
-                    {commentItem}
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments = {props.dish.comments} />
                 </div>
             </div>
         )
     }
-}
 
-export default Dishdetail
+export default DishDetail

a10ae4dfd2f8bfaae5081e7fa26f35c273fecf71
hardikshah197 <hardikshah.hs2015@gmail.com>
2020-06-16 21:35:55 +0530

    Control Form

---
+++ src/components/menuComponent.js
@@ -0,0 +1,55 @@
+import React,{ Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import myDishDetailComponent from './myDishDetailComponent'
+
+class Menu extends Component {
+     
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null
+        }
+    }
+    OnDishSelect(dish) {
+        this.setState({ selectedDish : dish});
+    }
+    renderDish = (dish) => {
+        if (dish != null) {
+            return (
+                <myDishDetailComponent dish={dish}></myDishDetailComponent>
+            )
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
+    }
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.OnDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay className="ml-5">
+                            <CardTitle >
+                                {dish.name}
+                            </CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+        return (
+            <div className="container">
+                <div className="row">
+                {menu}
+                </div>
+                <div>
+                    {this.renderDish(this.state.selectedDish)}
+                </div>
+            </div>
+        );
+    }
+}
+export default Menu;

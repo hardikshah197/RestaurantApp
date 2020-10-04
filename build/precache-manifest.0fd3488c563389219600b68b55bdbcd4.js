123e407b856232bace01c335e99a2f80b3be91a3
hardikshah197 <hardikshah.hs2015@gmail.com>
2020-10-04 17:31:46 +0530

    build added

---
+++ build/precache-manifest.0fd3488c563389219600b68b55bdbcd4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c79365840fb74c3c30271bbe485bc37",
+    "url": "/hardikshah197/RestaurantAppindex.html"
+  },
+  {
+    "revision": "dd6b20890dfda3a92944",
+    "url": "/hardikshah197/RestaurantAppstatic/css/2.04109cdd.chunk.css"
+  },
+  {
+    "revision": "9e309cc229633e1ee57f",
+    "url": "/hardikshah197/RestaurantAppstatic/css/main.67da04d8.chunk.css"
+  },
+  {
+    "revision": "dd6b20890dfda3a92944",
+    "url": "/hardikshah197/RestaurantAppstatic/js/2.69c68e9c.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/hardikshah197/RestaurantAppstatic/js/2.69c68e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e309cc229633e1ee57f",
+    "url": "/hardikshah197/RestaurantAppstatic/js/main.990c9bb1.chunk.js"
+  },
+  {
+    "revision": "62657b28168d76b71da9",
+    "url": "/hardikshah197/RestaurantAppstatic/js/runtime-main.74950973.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hardikshah197/RestaurantAppstatic/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hardikshah197/RestaurantAppstatic/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hardikshah197/RestaurantAppstatic/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hardikshah197/RestaurantAppstatic/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hardikshah197/RestaurantAppstatic/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
